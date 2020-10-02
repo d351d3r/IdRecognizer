0e54714e6b90dc0d73b54b22303a892eb611811b
RepearedApple <d.lysyenko@ya.ru>
2020-10-02 23:40:11 +1000

    Сделал тест консольного

---
+++ ConsoleDotNetTest/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Recognizer;
+namespace ConsoleDotNetTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var testIMG = @"./4.jpg";
+            Recognize recognize = new Recognize();
+            recognize.MRZ(testIMG);
+        }
+    }
+}
