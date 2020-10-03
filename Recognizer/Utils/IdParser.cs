35486f117fbaaaa6efddbb82c6b51760b5f8897f
Fedor Ishchenko <aleks180700@gmail.com>
2020-10-03 18:12:34 +1000

    Парсер + соответствие

--- Recognizer/Utils/IdParser.cs
+++ Recognizer/Utils/IdParser.cs
@@ -8,13 +8,14 @@ namespace RecognizerDLL.Utils
 {
 	public static class IdParser
 	{
-		private static string Sample = " Result: {\"duration\":117,\"frame_id\":0,\"zones\":[{\"lines\":[{\"confidence\":91.0,\"text\":\"PNRUSIWNKO<<FDOR<SRG**VI3<<<<<<<<<<<<<<<<\",\"warpedBox\":[-1.0,-1.0,-1.0,-1.0,-1.0,-1.0,-1.0,-1.0]},{\"confidence\":90.0,\"text\":\"02750277RUS000710M<<<<<<<0200*072\",\"warpedBox\":[-1.0,-1.0,-1.0,-1.0,-1.0,-1.0,-1.0,-1.0]}],\"warpedBox\":[283,2217,1765,2217,1765,2373,283,2373]}]}";
+		private static string Sample = "Result: {\"duration\":117,\"frame_id\":0,\"zones\":[{\"lines\":[{\"confidence\":91.0,\"text\":\"PNRUSIWNKO<<FDOR<SRG**VI3<<<<<<<<<<<<<<<<\",\"warpedBox\":[-1.0,-1.0,-1.0,-1.0,-1.0,-1.0,-1.0,-1.0]},{\"confidence\":90.0,\"text\":\"02750277RUS000710M<<<<<<<0200*072\",\"warpedBox\":[-1.0,-1.0,-1.0,-1.0,-1.0,-1.0,-1.0,-1.0]}],\"warpedBox\":[283,2217,1765,2217,1765,2373,283,2373]}]}";
 
 		public static void Parse(string jsonStr)
 		{
 			jsonStr = Sample;
 
 			var p = Split(jsonStr);
+			var d = p.Birth;
 		}
 
 		private static PersonId Split(string jsonStr)
@@ -26,33 +27,33 @@ namespace RecognizerDLL.Utils
 			//var ans = Regex.Match()
 
 			var namesInput = firstParse.Split("<").Where(x => x != "");
-			var secondName = namesInput.ElementAt(0).Skip(5);
+			var secondName = namesInput.ElementAt(0).Skip(5).ToArray();
 			var name = namesInput.ElementAt(1);
 			var middleName = namesInput.ElementAt(2);
 
 			var secondPhraseParts = secondPhrase.Split("<").Where(x => x != "");
 
 			var firstPart = secondPhraseParts.ElementAt(0);
-			var series = firstPart.Take(3);
-			var number = firstPart.Skip(3).Take(6);
-			var birth = firstPart.Skip(3 + 6 + 4).Take(6);
-			var gender = firstPart.Skip(3 + 6 + 4 + 6 + 1).Take(1);
+			var series = firstPart.Take(3).ToArray();
+			var number = firstPart.Skip(3).Take(6).ToArray();
+			var birth = firstPart.Skip(3 + 6 + 4).Take(6).ToArray();
+			var gender = firstPart.Skip(3 + 6 + 4 + 6 + 1).Take(1).ToArray();
 
-			var secongPart = secondPhraseParts.ElementAt(1);
-			var expiration = secongPart.Skip(1).Take(6);
-			var code = secongPart.Skip(1 + 6).Take(6);
+			var secongPart = secondPhraseParts.ElementAt(1).ToArray();
+			var expiration = secongPart.Skip(1).Take(6).ToArray();
+			var code = secongPart.Skip(1 + 6).Take(6).ToArray();
 
 			return new PersonId
 			{
-				Name = name,
-				SecondName = secondName.ToString(),
-				MiddleName = middleName.ToString(),
-				Series = series.ToString(),
-				Number = number.ToString(),
-				Birth = birth.ToString(),
-				Gender = gender.ToString(),
-				Code = code.ToString(), 
-				Expiration = expiration.ToString(),
+				Name = name.ToRus(),
+				SecondName = new string(secondName).ToRus(),
+				MiddleName = middleName.ToRus(),
+				Series = new string(series),
+				Number = new string(number),
+				Birth = new string(birth),
+				Gender = new string(gender),
+				Code = new string(code), 
+				Expiration = new string(expiration),
 			};
 		}
 	}
